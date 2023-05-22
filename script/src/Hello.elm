74db708f9dc249644440abdd0a4f97845e3e2442
Aksel Wester <aksel@wester.co>
2023-05-22 14:57:45 +0200

    Legg til decoding av exposed modules

--- script/src/Hello.elm
+++ script/src/Hello.elm
@@ -2,17 +2,34 @@ module Hello exposing (run)
 
 import BackendTask
 import BackendTask.Http
+import Dict
 import FatalError exposing (FatalError)
 import Json.Decode exposing (Decoder)
 import Pages.Script as Script exposing (Script)
 
 
-packageDescriptionDecoder : Decoder String
+type alias PackageDescription =
+    { name : String, exposedModules : List String }
+
+
+packageDescriptionDecoder : Decoder PackageDescription
 packageDescriptionDecoder =
-    Json.Decode.field "summary" Json.Decode.string
+    Json.Decode.map2 PackageDescription
+        (Json.Decode.field "name" Json.Decode.string)
+        (Json.Decode.field "exposed-modules" exposedModulesDecoder)
 
 
-getPackageDescription : String -> BackendTask.BackendTask FatalError String
+exposedModulesDecoder : Decoder (List String)
+exposedModulesDecoder =
+    Json.Decode.oneOf
+        [ Json.Decode.list Json.Decode.string
+        , Json.Decode.dict (Json.Decode.list Json.Decode.string)
+            |> Json.Decode.map Dict.values
+            |> Json.Decode.map List.concat
+        ]
+
+
+getPackageDescription : String -> BackendTask.BackendTask FatalError PackageDescription
 getPackageDescription packageName =
     BackendTask.Http.getJson
         ("https://package.elm-lang.org/packages/" ++ packageName ++ "/latest/elm.json")
@@ -24,5 +41,5 @@ run : Script
 run =
     Script.withoutCliOptions
         (getPackageDescription "elm/html"
-            |> BackendTask.andThen (\s -> Script.log s)
+            |> BackendTask.andThen (\s -> Script.log (Debug.toString s))
         )
