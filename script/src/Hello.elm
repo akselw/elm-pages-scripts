d7d5a46e24fd48f581e5cfbe91c3b5ca7b68ca37
Aksel Wester <aksel@wester.co>
2023-05-22 14:45:44 +0200

    Legg til henting av package description fra package.elm-lang.org

--- script/src/Hello.elm
+++ script/src/Hello.elm
@@ -1,7 +1,28 @@
 module Hello exposing (run)
 
+import BackendTask
+import BackendTask.Http
+import FatalError exposing (FatalError)
+import Json.Decode exposing (Decoder)
 import Pages.Script as Script exposing (Script)
+
+
+packageDescriptionDecoder : Decoder String
+packageDescriptionDecoder =
+    Json.Decode.field "summary" Json.Decode.string
+
+
+getPackageDescription : String -> BackendTask.BackendTask FatalError String
+getPackageDescription packageName =
+    BackendTask.Http.getJson
+        ("https://package.elm-lang.org/packages/" ++ packageName ++ "/latest/elm.json")
+        packageDescriptionDecoder
+        |> BackendTask.allowFatal
+
 
 run : Script
 run =
-    Script.withoutCliOptions (Script.log "Hello, Elm-faggruppa!")
+    Script.withoutCliOptions
+        (getPackageDescription "elm/html"
+            |> BackendTask.andThen (\s -> Script.log s)
+        )
