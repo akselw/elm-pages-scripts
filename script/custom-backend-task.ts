23bc97c927d698bbe3b1cf264ddff5776791dbfb
Aksel Wester <aksel@wester.co>
2023-09-28 13:45:56 +0200

    Legg til timeout mellom faneåpninger, for å bevare rekkefølgen

--- script/custom-backend-task.ts
+++ script/custom-backend-task.ts
@@ -2,4 +2,5 @@ import { exec } from 'child_process'
 
 export async function openChrome(url) {
     await exec(`open -a "Google Chrome Canary" ${url}`);
+    await new Promise(r => setTimeout(r, 150));
 }
